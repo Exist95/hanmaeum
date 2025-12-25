b1fa05e4944e04bfd4a44383f6ffea89246f5b2e
Exist95 <jyeol0210@naver.com>
2025-12-25 18:00:16 +0900

    날짜 변경

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -83,7 +83,7 @@ export const getSortedPostList = async (category?: string) => {
 
 export const getSitemapPostList = async () => {
   const postList = await getPostList();
-  const baseUrl = 'https://www.d5br5.dev';
+  const baseUrl = 'https://hanma-eum.vercel.app';
   const sitemapPostList = postList.map(({ url }) => ({
     lastModified: new Date(),
     url: `${baseUrl}${url}`,
